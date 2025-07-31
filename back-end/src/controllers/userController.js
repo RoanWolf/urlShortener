d9e661a1c3650bd895f0bfa351e12ab69763c6df
neq <neq17320251@gmail.com>
2025-07-31 23:39:06 +0800

    注册用户规范

--- back-end/src/controllers/userController.js
+++ back-end/src/controllers/userController.js
@@ -1,21 +1,37 @@
 import User from "../models/userModel.js";
-
-export async function createUser(req, res) {
-  const information = req.body;
+// 注册账号
+export async function createUser1(req, res) {
+  const information = req.body; // { username, password } username is unique
   try {
     await User.create(information);
     return res.status(201).json({
-      message: "User created successfully"
+      message: "User created successfully",
     });
   } catch (error) {
     if (error.name === "SequelizeUniqueConstraintError") {
-      res
-        .status(400)
-        .json({ message: "Name must be unique. This name is already taken." });
+      return res.status(400).json({ message: "Username already exists" });
     } else {
-      res.status(500).json({ message: "An error occurred." });
+      return res.status(500).json({ message: "An error occurred." });
     }
   }
 }
+export async function createUser(req, res) {
+  const information = req.body; // { username, password } username is unique
+  try {
+    const ok = await User.findOne({
+      where: { username: information.username },
+    });
+    if (ok) {
+      return res.status(400).json({
+        message: "Username already exists",
+      });
+    }
+    await User.create(information);
+    return res.status(201).json({
+      message: "User created successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "An error occurred." });
+  }
+}
 
-export async function getUserByUsernamer(req, res) {}
