ab7f82b2e5ae9a358e31dfbd199e453610695ea3
neq <neq17320251@gmail.com>
2025-08-01 23:08:41 +0800

    encrypt

--- back-end/src/controllers/userController.js
+++ back-end/src/controllers/userController.js
@@ -1,17 +1,20 @@
-import User from "../models/userModel.js";
+import {
+  createUser as createUserApi,
+  hasUsername as hasUsernameApi,
+  verifyUser as verifyUserApi
+} from "../services/userService.js";
 
 export async function createUser(req, res) {
-  const information = req.body; // { username, password } username is unique
+  const information = req.body;
+
   try {
-    const ok = await User.findOne({
-      where: { username: information.username },
-    });
+    const ok = hasUsernameApi(information.username);
     if (ok) {
       return res.status(400).json({
         message: "Username already exists",
       });
     }
-    await User.create(information);
+    await createUserApi(information);
     return res.status(201).json({
       message: "User created successfully",
     });
@@ -22,15 +25,14 @@ export async function createUser(req, res) {
 
 export async function verifyUser(req, res) {
   const { username, password } = req.body;
-    try {
-        const user = await User.findOne({
-        where: { username, password },
-        });
-        if (!user) {
-        return res.status(401).json({ message: "Invalid credentials" });
-        }
-        return res.status(200).json({ message: "Login successful" });
-    } catch (error) {
-        return res.status(500).json({ message: "An error occurred." });
+
+  try {
+    const isUser = await verifyUserApi(username, password);
+    if (!isUser) {
+      return res.status(401).json({ message: "Invalid credentials" });
     }
+    return res.status(200).json({ message: "Login successful" });
+  } catch (error) {
+    return res.status(500).json({ message: "An error occurred." });
+  }
 }
